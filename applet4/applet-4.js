e72cd80184eaf28aa7bbea176ce2f83ea717dc97
jaamee-s <anticamarajame@gmail.com>
2024-10-18 22:19:56 +0800

    added a fetchData

---
+++ applet4/applet-4.js
@@ -0,0 +1,33 @@
+class StudentList {
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.students = [];
+        this.init();
+    }
+
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    renderStudentList(students) {
+        const studentSearchListContainer = document.getElementById('studentSearchList');
+        studentSearchListContainer.innerHTML = ''; 
+
+        students.forEach(student => {
+            studentSearchListContainer.innerHTML += `
+                <p> ${student.student_name}</p>
+                <p class="fw-light"> ${student.student_program} </p>
+                <hr>
+            `;
+        });
+    }
