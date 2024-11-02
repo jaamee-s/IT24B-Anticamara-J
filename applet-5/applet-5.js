886d4f997a2511444dd5d629d0779b55027cb81a
jaamee-s <anticamarajame@gmail.com>
2024-11-02 18:09:50 +0800

    added the weatherCard

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -9,3 +9,15 @@ class WeatherApp {
 
         //Geolocation Input
         this.getLocationBtn = document.getElementById('getLocationBtn');
+
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+
+        //Event Listener
+        this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+        this.getLocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
+    }
